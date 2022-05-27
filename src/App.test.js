492ef9c46b0d8988a8687f890c2037939de787b5
naturelle <ebruekici@gmail.com>
2022-05-27 17:09:21 +0300

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,21 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('have list ', () => {
+  render(<App />);
+  const listItems = screen.getAllByRole("listitem");
+  expect(listItems.length).toBe(3);
+});
+
+test('renders title', () => {
+  render(<App />);
+  const title = screen.getByTestId("mytestid");
+  expect(title).toBeInTheDocument(3);
+});
+
+test('sum should be 5', () => {
+  render(<App />);
+  const sum = screen.getByTitle("sum");
+  expect(sum.textContent).toBe("5");
+});
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+const Login = () => {
+    return (
+        <div className="container">
+            <form>
+                <input type="text"/>
+                <input type="password"/>
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
+export default Login;
